a6d96dc0bc48f6ea791b00e32922b4f925962156
Nabeelep <84263179+Nabeelep@users.noreply.github.com>
2022-08-12 22:15:57 +0530

    created backend and just started the frontend

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+
+//passwrd - eESFhvpO41cDnP0v
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/book-routes');
+
+
+
+const app = express();
+
+//Middlewares
+app.use(express.json());
+app.use('/books',router)//localhost:5000/books
+
+
+
+
+mongoose.connect(
+    "mongodb+srv://admin:eESFhvpO41cDnP0v@cluster0.qq3ghxd.mongodb.net/bookStore?retryWrites=true&w=majority")
+    .then(()=>console.log("connected to database"))
+    .then(()=>{
+        app.listen(5000)
+    }).catch((err) => console.log("error occured"+err));
