332e9cab3e6383a78b794afd507cb7bacb5472fc
Nabeelep <84263179+Nabeelep@users.noreply.github.com>
2022-08-13 08:14:03 +0530

    connected the backend to the frontend and fetch the data

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/book-routes');
+const cors = require('cors')
 
 
 
@@ -10,6 +11,7 @@ const app = express();
 
 //Middlewares
 app.use(express.json());
+app.use(cors())
 app.use('/books',router)//localhost:5000/books
 
 
